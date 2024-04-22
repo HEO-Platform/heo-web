8cc2476a6160ebbc1ce7f9b9d45932cf36204982
Greg Solovyev <fiddlestring@gmail.com>
2024-04-21 23:10:42 -0700

    make tip amount 5

--- src/components/CampaignPage.js
+++ src/components/CampaignPage.js
@@ -1287,9 +1287,9 @@ class CampaignPage extends Component {
             this.props.history.push("/404");
             return;
         }
-        this.state.donationAmount = campaign.defaultDonationAmount ? campaign.defaultDonationAmount : "10";
-        this.state.tipAmount = 2;
-        this.state.totalAmount = 12;
+        this.state.donationAmount = campaign.defaultDonationAmount ? campaign.defaultDonationAmount : "20";
+        this.state.tipAmount = 5;
+        this.state.totalAmount = campaign.defaultDonationAmount + 5;
         campaign.percentRaised = 100 * (this.state.raisedAmount)/campaign.maxAmount;
         var contentState = {};
         var lng
